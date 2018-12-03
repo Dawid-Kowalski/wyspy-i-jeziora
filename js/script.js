6146bedb851dbe030461da50a3f555e1807188f4
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-03 18:22:25 +0100

    Add responsive to google maps

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-function showPlace() {
+let choosenPlace;
 
+function showPlaceInfo () {
+	document.getElementById("place-name").innerHTML = placeData[choosenPlace-1].name;
+  	document.getElementById("description").innerHTML = placeData[choosenPlace-1].description;
+   	document.getElementById("map").innerHTML = placeData[choosenPlace-1].map;
+
+}
+
+function checkPlace () {
 	for(let i=1; i<7; i++) {
 		if(document.getElementById("place"+i).checked) {
    			choosenPlace = document.getElementById("place"+i).value;
    		}
    }
-   
-   document.getElementById("place-name").innerHTML = placeData[choosenPlace-1].name;
-   document.getElementById("description").innerHTML = placeData[choosenPlace-1].description;
-   document.getElementById("map").innerHTML = placeData[choosenPlace-1].map;
+}
 
+function showPlace() {
+	checkPlace();
+	showPlaceInfo();
 }
 
 let placeData = [
