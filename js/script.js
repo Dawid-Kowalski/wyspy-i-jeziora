39416229deaa8418f0d4738e738e63c92b977462
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-03 17:49:47 +0100

    Add place data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 function showPlace() {
 
 	for(let i=1; i<7; i++) {
-		if(document.getElementById('place'+i).checked) {
-   			choosenPlace = document.getElementById('place'+i).value;
+		if(document.getElementById("place"+i).checked) {
+   			choosenPlace = document.getElementById("place"+i).value;
    		}
    }
    alert(choosenPlace);
 }
+
+let placeData = [
+	{
+		name: "Morze Kaspijskie",
+		description: "Morze Kaspijskie – bezodpływowe słone jezioro reliktowe w Azji i w części północnej w Europie. Jest największym jeziorem świata z powierzchnią wynoszącą ok. 370 tys. km² (zmieniającą się wskutek wahań poziomu wody, w 1930 roku wynosiła ona 442 tys. km²). Maksymalna głębokość to 1025 m. Zasolenie od słodkowodnej części północnej do 10–12‰ w części środkowej i południowej i do bardzo dużego, sięgającego nawet 300‰ w zamkniętej zatoce Kara-Bogaz-Goł. Czas wymiany wód wynosi 250 lat.",
+		map: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12240890.2567431!2d41.398224695362345!3d41.49346880795777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f88155c8ff7a61f%3A0x519e69c2d4808ffe!2sMorze+Kaspijskie!5e0!3m2!1spl!2sde!4v1543854824427' width='400' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>"
+	},
+	{
+		name: "Grenlandia",
+		description: "Dutonomiczne terytorium zależne Danii położone na wyspie o tej samej nazwie w Ameryce Północnej, o obszarze 2 166 086 km² (największa wyspa na świecie niebędąca kontynentem), pokrytej w 81% przez lądolód grenlandzki (410,4 tys. km² jest wolnych od lodu) i o ludności 56 282 (styczeń 2014) 89% mieszkańców stanowią Inuici." ,
+		map: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24638142.02411952!2d-73.8427586405187!3d67.85773665996899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ea20dbbe3c07715%3A0x34cf9d830114e218!2sGrenlandia!5e0!3m2!1spl!2sde!4v1543854525471' width='400' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>"
+	},
+	{
+		name: "Nettilling Lake",
+		description: "Jezioro w Kanadzie, położone w południowej części Ziemi Baffina na wysokości 30 m n.p.m. na równinie Great Plain of the Koukdjuak. Zajmuje powierzchnię 5542 km². Jezioro odwadnia rzeka Koukdjuak, uchodząca do Basenu Foxe'a. Jezioro przez większą część roku pokryte jest lodem",
+		map: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199360.5981218047!2d-78.23748244443637!3d66.88568530872186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4e7814a8c85a3bbb%3A0x47979f3d781e2e37!2sNettilling+(jezioro)!5e0!3m2!1spl!2sde!4v1543855145122' width='400' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>"
+	},
+	{
+		name: "Manitoulin Island",
+		description: "Kanadyjska wyspa, w prowincji Ontario, położona w północnej części jeziora Huron. Od stałego lądu oddziela ją North Channel. Zajmuje powierzchnię 2766 km² i jest największą na świecie wyspą położoną na jeziorze. Jest 172. co do wielkości wyspą na Ziemi oraz 31. co do wielkości wyspą Kanady.",
+		map: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d712651.8863649268!2d-82.96242197891318!3d45.75522689727346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4d323b9a7ed851e1%3A0xaeb4a12150af6342!2sManitoulin+(wyspa)!5e0!3m2!1spl!2sde!4v1543855344071' width='400' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>"
+	},
+	{
+		name:  "Lake Manitou",
+		description: "największe jezioro na wyspie Manitoulin w Kanadzie. Jego powierzchnia wynosi około 104 km². Jest największym na świecie jeziorem na wyspie położonej na innym jeziorze (Huron). Nazywane jest z tego powodu 'jeziorem w jeziorze'." ,
+		map: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d178078.49204762693!2d-82.13057472013838!3d45.78168195712092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4d32062241b1962d%3A0xbeff4ab9728595ed!2sManitou!5e0!3m2!1spl!2sde!4v1543855569657' width='400' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>"
+	},
+	{
+		name:  "Pulau Samosir",
+		description: "Wyspa na jeziorze Danau Toba na Sumatrze",
+		map: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255086.74448992652!2d98.6980128744737!3d2.593882457346116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031e8022c23c201%3A0x9e185a1f1c3da745!2sSamosir!5e0!3m2!1spl!2sde!4v1543855700040' width='400' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>"
+	}
+];
